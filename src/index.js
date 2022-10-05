c3e9a1f9fd9b8723250a826e43f99e7419854b19
Doheera-kosi <kupourevans99@gmail.com>
2022-10-05 14:32:07 +0000

    Modify src/index.js

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import _ from 'lodash';
+import _, { forEach } from 'lodash';
 import './style.css';
 
+const dSet = [
+  {
+    desc: 'Wash dishes',
+    completed: false,
+    index: '0',
+  },
+  {
+    desc: 'Attend Evening service for church prayer meeting',
+    completed: false,
+    index: '1',
+  },
+  {
+    desc: 'Attend community development meeting',
+    completed: false,
+    index: '2',
+  },
+  {
+    desc: 'Attend Morning Session',
+    completed: false,
+    index: '3',
+  }
+]
+
 function component() {
-  const element = document.createElement('div');
-
+  const element = document.createElement('li');
   // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  element.classList.add('hello');
-
-
+  dSet.forEach((d, index) => {
+    if(d.index == index) {
+      
+      element.innerHTML += `
+      <div class='li-item'>
+        <div>
+          <input type="checkbox">
+          <label for="text">${d.desc}</label>
+        </div>
+        <div><i class="fa fa-ellipsis-v" aria-hidden="true"></i></div>
+      </div>
+      <hr>
+      `;
+    }
+  })
+  element.classList.add('item');
   return element;
 }
-
-document.body.appendChild(component());
+document.querySelector('.list').appendChild(component());
