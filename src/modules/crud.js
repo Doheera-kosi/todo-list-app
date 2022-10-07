bbf0ab7e101f2bda10c57d99c7325e397980f45a
Doheera-kosi <kupourevans99@gmail.com>
2022-10-07 12:09:01 +0000

    Modify style.js

--- src/modules/crud.js
+++ src/modules/crud.js
@@ -10,3 +10,24 @@ export function status(elem, list) {
   });
   saveLocal(list);
 }
+
+// New
+export function add(list) {
+  list.push({ description: document.querySelector('#input-field').value, isCompleted: false, index: list.length });
+  document.querySelector('#input-field').value = '';
+  saveLocal(list);
+}
+
+export function updateIndex(list) {
+  let i = 0;
+  list.forEach((elem) => {
+    elem.index = i;
+    i += 1;
+  });
+}
+
+export function removeDone(list) {
+  list = list.filter((elem) => elem.isCompleted === false);
+  updateIndex(list);
+  saveLocal(list);
+}
